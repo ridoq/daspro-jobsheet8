2823ac2a6da639fe041c36e943fbb36ed6c38629
ridoq <ridoq444@gmail.com>
2025-10-31 15:21:56 +0700

    Percobaan 4

---
+++ RataNilai24.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class RataNilai24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,j;
+        float nilai, totalNilai, rataNilai;
+        i = 1;
+        while (i <= 5) {
+            System.out.println("Input nilai Mahasiswa ke " + i);
+            totalNilai = 0;
+            for (j=1; j <= 5; j++) {
+                System.out.print("Nilai ke-" + j + " = ");
+                nilai = sc.nextFloat();
+                totalNilai += nilai;
+
+            }
+            rataNilai = totalNilai / 5;
+            System.out.println("Rata-rata Nilai Mahasiswa ke " + i + " adalah " + rataNilai);
+            System.out.println("-----------------------------------------------------------");
+            i++;
+        }
+        sc.close();
+    }
+}
